c37679e9f64a058446ec673c13e39bc9e32e1841
luthfiya13 <fuadahluthfiya@gmail.com>
2023-05-11 08:58:47 +0700

    instal daisy, routing, dan landing page(Luthfiya)

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Switch, Route, NavLink, Redirect, Link } from "react-router-dom"; 
+import landingpage from './pages/landingpage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+      <Route component={landingpage} path="/" exact/>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/landingpage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import icon from '../images/icons.png'
+
+const landingpage = () => {
+  return (
+    <div className="place-content-evenly bg-indigo-900 h-screen" style={{ backgroundColor: '#000E2B' }}>
+  <div style={{ backgroundColor: '#030812', height: '80px', display: 'flex', justifyContent: 'flex-start', alignItems: 'center' }}>
+    <div className="ml-5" style={{ display: 'flex', alignItems: 'center' }}>
+      <img src={icon} width="50px" height="50px" />
+      <span className="ml-3 text-white">Kelompok 4</span>
+      <div className="ml-auto">
+        <button className="btn btn-outline">Login</button>
+      </div>
+    </div>
+  </div>
+</div>
+  )
+}
+
+export default landingpage
